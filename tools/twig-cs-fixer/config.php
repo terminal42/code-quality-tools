7d5369f01ffd8294ac3900b78d2b74faa1453486
Yanick Witschi <yanick.witschi@terminal42.ch>
2026-07-17 13:55:01 +0200

    Fix quality tools

--- tools/twig-cs-fixer/config.php
+++ tools/twig-cs-fixer/config.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Contao\CoreBundle\Twig\Defer\DeferTokenParser;
 use Contao\CoreBundle\Twig\ResponseContext\AddTokenParser;
 use Contao\CoreBundle\Twig\Slots\SlotTokenParser;
